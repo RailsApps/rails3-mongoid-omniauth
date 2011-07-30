7c08a19a508c12dea9ac13852f069a039447a38b
Daniel Kehoe <kehoe@fortuity.com>
2011-07-30 13:34:13 -0700

    conform to app generated by template for Rails 3.1.0.rc5

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'http://rubygems.org'
-gem 'rails', '3.1.0.rc4'
-gem 'sass-rails', "~> 3.1.0.rc"
-gem 'coffee-script'
-gem 'uglifier'
+gem 'rails', '3.1.0.rc5'
+group :assets do
+  gem 'sass-rails', "~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
 gem 'jquery-rails'
 gem "rspec-rails", ">= 2.6.1", :group => [:development, :test]
 gem "database_cleaner", ">= 0.6.7", :group => :test
--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,10 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 # # require "rails/test_unit/railtie"
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+# If you have a Gemfile, require the default gems, the ones in the
+# current environment and also include :assets gems if in development
+# or test environments.
+Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module Rails3MongoidOmniauth
   class Application < Rails::Application
