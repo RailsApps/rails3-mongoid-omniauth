712e1b30b5d73c3e690eba5574ee64e1d332b440
Daniel Kehoe <daniel@danielkehoe.com>
2011-11-08 14:50:25 -0800

    better way to set the user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,8 @@ class User
       user.provider = auth['provider']
       user.uid = auth['uid']
       if auth['info']
-        user.name = auth['info']['name'] if auth['info']['name'] # Twitter, Google, Yahoo, GitHub
-        user.email = auth['info']['email'] if auth['info']['email'] # Google, Yahoo, GitHub
-      end
-      if auth['extra'] && auth['extra']['user_hash']
-        user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
-        user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
+         user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || ""
       end
     end
   end
