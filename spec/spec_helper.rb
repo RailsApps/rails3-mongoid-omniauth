4cd7b56a292953be833a719ec969ec657c567b44
Daniel Kehoe <daniel@danielkehoe.com>
2012-02-21 20:25:43 -0800

    conform to version generated by version 1.1.4 of rails_apps_composer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'email_spec'
 require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -9,7 +10,9 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # == Mock Framework
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+  # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
