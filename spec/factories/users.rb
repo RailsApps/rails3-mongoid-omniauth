b3332afa549f6e574be24121a313100c34d6e279
Daniel Kehoe <daniel@danielkehoe.com>
2012-04-03 09:43:10 -0700

    conform to app generated by version 1.4.3 of rails_apps_composer

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    provider "MyString"
+    uid "MyString"
+    name "MyString"
+    email "MyString"
+  end
+end
