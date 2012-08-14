1841ce41b2e1afee57e46e5370d6ad64a5f77019
Daniel Kehoe <daniel@danielkehoe.com>
2012-08-13 17:08:20 -0700

    build using rails_apps_composer 2.1.4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
 end
