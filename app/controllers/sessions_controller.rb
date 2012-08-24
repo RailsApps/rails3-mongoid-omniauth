efd9a0d9ba9502f41621aacafbdd4c297302db55
Daniel Kehoe <daniel@danielkehoe.com>
2012-08-24 12:23:01 -0700

    resolve https://github.com/RailsApps/rails-composer/issues/6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.where(:provider => auth['provider'], 
-                      :uid => auth['uid']).first || User.create_with_omniauth(auth)
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     if user.email.blank?
       redirect_to edit_user_path(user), :alert => "Please enter your email address."
