223e3c9b08a0381a791cf46fb71f7d052cf7ba92
Daniel Kehoe <daniel@danielkehoe.com>
2012-08-27 16:43:40 -0700

    adding user#index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :correct_user?
+  before_filter :correct_user?, :except => [:index]
 
   def index
     @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails3MongoidOmniauth::Application.routes.draw do
   root :to => "home#index"
-  resources :users, :only => [ :show, :edit, :update ]
+  resources :users, :only => [:index, :show, :edit, :update ]
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
