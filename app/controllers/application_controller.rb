9e028711eb0d247c38c2bf5da035b2eebaef571c
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-08 16:49:48 -0800

    build using rails_apps_composer 2.2.20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     def current_user
       begin
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      rescue Mongoid::Errors::DocumentNotFound
+      rescue Exception => e
         nil
       end
     end
