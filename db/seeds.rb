ced94e55cd622639602fa04b82df94afd7e44c07
Daniel Kehoe <daniel@danielkehoe.com>
2013-01-20 18:41:13 -0800

    build using rails_apps_composer 2.2.30

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Environment variables (ENV['...']) are set in the file config/application.yml.
+# See http://railsapps.github.com/rails-environment-variables.html
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UsersController do
 
   before do
-    @user = create(:user)
+    @user = FactoryGirl.create(:user)
   end
 
   describe "Get 'show' for non-logged in user" do
@@ -16,7 +16,7 @@ describe UsersController do
 
   describe "Get 'show' for another user" do
     it "redirects user to root path with access denied" do
-      @user2 = create(:user)
+      @user2 = FactoryGirl.create(:user)
       session[:user_id] = @user.id
       get 'show', :id => @user2
       response.should redirect_to(root_path)
